712def0799bb0fea7421d0db8be911260e7358db
ahmadEssam77 <ahmad77essam@gmail.com>
2021-09-14 15:17:04 +0200

    Make the project in one file html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if(document.querySelector("body").classList.contains("primary-bg-color")){
-    let user = prompt("Write your name to welcome you xD", "Ahmad");
+    let user = prompt("Write your name to welcome you xD");
     let welcomeUserDisplay = document.querySelector(".welcomeUserDisplay");
 
     welcomeUserDisplay.innerHTML = `Welcome ${user} :)`;
