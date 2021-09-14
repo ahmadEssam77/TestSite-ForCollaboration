cc330a106777b15ba144c6a810162a956d20c36b
Aya Kassem <aya.kassem66@gmail.com>
2021-09-14 13:11:24 +0200

    added new files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-let user = prompt("Write your name to welcome you xD", "Ahmad");
-let welcomeUserDisplay = document.querySelector(".welcomeUserDisplay");
+if(document.querySelector("body").classList.contains("primary-bg-color")){
+    let user = prompt("Write your name to welcome you xD", "Ahmad");
+    let welcomeUserDisplay = document.querySelector(".welcomeUserDisplay");
 
-welcomeUserDisplay.innerHTML = `Welcome ${user} :)`;
+    welcomeUserDisplay.innerHTML = `Welcome ${user} :)`;
+}
+else{
+    function displayTime(){
+        let currentTime = new Date().toLocaleTimeString();
+        document.querySelector("#demo").innerHTML = currentTime;
+    }
+    
+    setInterval(displayTime)
+}
+
+
+
+// ===============================
