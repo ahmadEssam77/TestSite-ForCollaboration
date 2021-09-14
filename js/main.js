363f239d4b29ae104588ae8d10c15613076ed7cc
ahmadEssam77 <ahmad77essam@gmail.com>
2021-09-14 16:53:51 +0200

    Now the index file has all functions that were in time file

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-if(document.querySelector("body").classList.contains("primary-bg-color")){
-    let user = prompt("Write your name to welcome you xD");
-    let welcomeUserDisplay = document.querySelector(".welcomeUserDisplay");
 
-    welcomeUserDisplay.innerHTML = `Welcome ${user} :)`;
+let user = prompt("Write your name to welcome you xD");
+let welcomeUserDisplay = document.querySelector(".welcomeUserDisplay");
+
+welcomeUserDisplay.innerHTML = `Welcome ${user} :)`;
+
+function displayTime() {
+    let currentTime = new Date().toLocaleTimeString();
+    document.querySelector("#demo").innerHTML = currentTime;
 }
-else{
-    function displayTime(){
-        let currentTime = new Date().toLocaleTimeString();
-        document.querySelector("#demo").innerHTML = currentTime;
-    }
-    
-    setInterval(displayTime)
-}
+
+setInterval(displayTime)
+
 
 
 
