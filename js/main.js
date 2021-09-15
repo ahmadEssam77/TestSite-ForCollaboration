ef5b78fe91dc2b4aee9d78111f68d50fd299751e
ahmadEssam77 <ahmad77essam@gmail.com>
2021-09-15 12:00:10 +0200

    Added comments besides the code for more explanation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 (function (){
     let userName = prompt("Write your name to welcome you xD");
-    let firstCharacter = userName.charAt(0);
-    firstCharacter = firstCharacter.toUpperCase();
-    let slicingUserName = userName.slice(1);
-    slicingUserName = slicingUserName.toLowerCase();
-    userName = firstCharacter + slicingUserName;
+    let firstCharacter = userName.charAt(0);    // Detect or select the first letter of the user name
+    firstCharacter = firstCharacter.toUpperCase(); // Change it to be upper case style
+    let slicingUserName = userName.slice(1);  // Select or cut the whole user name except the first letter
+    slicingUserName = slicingUserName.toLowerCase(); // Make the rest of the name except the first letter to be lower case style
+    userName = firstCharacter + slicingUserName;  
 
 
     document.querySelector("h1").style.display = "block";
@@ -13,7 +13,7 @@
         welocmeUser(userName);
     }, 3000)
 
-})()
+})();
 
 
 function welocmeUser(userName){
